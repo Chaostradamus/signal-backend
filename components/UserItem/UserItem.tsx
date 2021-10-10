c5f62e55c15ca7466a27002b098e6c97d2b37895
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-10-09 21:56:34 -0400

    2forty five

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Text, Image, View, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import styles from "./styles";
+import { Auth, DataStore } from "aws-amplify";
+import { ChatRoom, User } from "../../src/models";
 
 export default function UserItem({ user }) {
   const navigation = useNavigation();
 
-  const onPress = () => {};
+  const onPress = async () => {
+    // const newChatRoom = await DataStore.save(new ChatRoom({ newMessages: 0 }));
+
+    const authUser = await Auth.currentAuthenticatedUser();
+    const dbUser = await DataStore.query(User, authUser.attributes.sub);
+    console.log(dbUser);
+  };
 
   return (
     <Pressable onPress={onPress} style={styles.container}>
--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -8,6 +8,18 @@ import { User } from "../src/models";
 export default function UsersScreen() {
   const [users, setUsers] = useState<User[]>([]);
 
+  useEffect(() => {
+    DataStore.query(User).then(setUsers);
+  }, []);
+  
+  //   useEffect(() => {
+  //     const fetchUsers = async () => {
+  //       const fetchedUsers = await DataStore.query(User);
+  //       setUsers(fetchedUsers);
+  //     };
+  //     fetchUsers();
+  //   }, []);
+
   return (
     <View style={styles.page}>
       <FlatList
